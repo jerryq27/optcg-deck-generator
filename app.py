ec02da960ad0f1eb5b197a602484accea3a8e3e5
Jerry <jerryq27+dev@gmail.com>
2025-05-02 17:10:51 -0500

    Getting image URLs based on card set id.

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ import httpx
 from bs4 import BeautifulSoup
 
 
-def get_card_lists():
+def get_card_sets():
     CARD_LIST_URL = "https://en.onepiece-cardgame.com/cardlist/?series=569001"
     response = httpx.get(CARD_LIST_URL)
 
     bsoup = BeautifulSoup(response.text, "lxml")
     options = bsoup.css.select("#series option", recursive=False)
-    print(f"Found {len(options)} options.")
+    # print(f"Found {len(options)} options.")
 
     card_sets = []
     for option in options:
@@ -17,28 +17,51 @@ def get_card_lists():
             continue
         card_sets.append({
             "id": option["value"],
-            "name": option.contents[0],
+            "name": option.contents[0].replace('<br class="spInline">', ''),
         })
 
+    return card_sets
+
+
+def get_card_set_images(card_set):
+
+    BASE_URL = "https://en.onepiece-cardgame.com"
+    DECK_URL = f"https://en.onepiece-cardgame.com/cardlist/?series={card_set['id']}"
+
+    response = httpx.get(DECK_URL)
+    bsoup = BeautifulSoup(response.text, "lxml")
+    images = bsoup.css.select("div.resultCol a img", recursive=False)
+
+    image_urls = [img["data-src"][2:] for img in images]
+
+    for image_url in image_urls:
+        print("/".join([BASE_URL, image_url]))
+
+    print(f"Cards: {len(image_urls)}")
+
+
+def show_ui(card_sets):
+    print("One Piece TCG Deck Generator")
     for i, card_set in enumerate(card_sets):
-        print(f"{i}: {card_set['id']} -> {card_set['name'].replace('<br class="spInline">', '')}")
+        print(f"{i}: {card_set['name']}")
 
-get_card_lists()
+    has_error = False
+    option = input("Select a card set: ")
+    try:
+        option = int(option)
+        card_set = card_sets[option]
+    except ValueError:
+        has_error = True
+        print(f"Error casting the value '{option}'")
+    except IndexError:
+        has_error = True
+        print(f"Invalid option '{option}'")
+    finally:
+        if has_error:
+            exit()
+    get_card_set_images(card_set)
 
-exit()
 
-BASE_URL = "https://en.onepiece-cardgame.com"
-DECK_URL = "https://en.onepiece-cardgame.com/cardlist/?series=569110"
+card_sets = get_card_sets()
+show_ui(card_sets)
 
-response = httpx.get(DECK_URL)
-print(response.status_code)
-
-bsoup = BeautifulSoup(response.text, "lxml")
-images = bsoup.css.select("div.resultCol a img", recursive=False)
-
-image_urls = [img["data-src"][2:] for img in images]
-
-for image_url in image_urls:
-    print("/".join([BASE_URL, image_url]))
-
-print(f"Cards: {len(image_urls)}")
